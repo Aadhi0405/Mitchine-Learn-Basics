8eea176436d9c6e0e2aaaa0a062e0bf2b90697a2
Aadhiseshan <aadhideveloper2000@gmail.com>
2022-07-26 20:40:47 +0530

    upload new python File

---
+++ basics.py
@@ -0,0 +1,99 @@
+##print("****************Arithmetic,Logic & Conditional Operators*****************") 
+##print(5+5)
+##
+##print(5*(2-7))
+##
+##print(5*5)
+##
+##print(5**5)
+##
+##print(40/2)
+##
+##print(40//2)
+##
+##print(100%2)
+##
+##print(10==10)
+##
+##print(5<4)
+##
+##print(5>4)
+##
+##print(5<=4)
+##
+##print(5>=4)
+##
+##no=5
+##
+##print(no)
+
+#====================================================================================================#
+##Implement String Operation
+
+##word=input("Enter the Any Word: ")
+##word1=word[::-1]
+
+##if word == word1:
+##    print(word," is  a palindrome")
+##else:
+##    print(word," is not palindrome")
+
+##file_location=r"\nome\tadhiseshan\GitHub_Learn"
+##print(file_location)
+
+#=====================================================================================================#
+
+#slice operator
+
+##name='ramachandran'
+##print('Your name Length is: '+str(len(name)))
+
+####print(name[0])
+####print(name[1])
+####print(name[2])
+####print(name[3])
+####print(name[4])
+####print(name[5])
+####print(name[6])
+####print(name[7])
+####print(name[8])
+####print(name[9])
+####print(name[10])
+####print(name[11])
+####print(name[12])
+##print(name[0].upper()+name[1:4]+name[4].upper()+name[5:len(name)-1]+name[-1].upper())
+##
+
+#=====================================================================================================#
+
+#step operator
+
+##name='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+##
+##print(len(name))
+##
+##print(name[0:len(name):2])
+
+#=====================================================================================================#
+#title() function 
+
+##news="government annonced the scheam"
+
+##print(news)
+##print(news.title())
+
+#=====================================================================================================#
+
+##name='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+##
+##print('A' and'KLMNO' in name)
+
+#=====================================================================================================#
+
+#Sorted() Funcution
+
+word1='swathi Akka'
+word2='Akka swathi'
+sorted(word1) == sorted(word2)
+
+print(sorted(word1))
